05db3dd36ca7b9f8d4cbe3a7310b28e49f268c7d
nisrinkh <204346333+nisrinkh@users.noreply.github.com>
2025-11-04 08:16:22 +0700

    update buat cart

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,6 +1,16 @@
 import { createApp } from 'vue';
+import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
 import App from './app.vue';
 import router from './router';
 import '../css/style.css';
+import axios from 'axios';
 
-createApp(App).use(router).mount('#app');
+
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+
+axios.defaults.baseURL = '/'
+axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('token')}`
+
+createApp(App).use(pinia).use(router).mount('#app');
