f23325688cfa6cf26c35694ed76d6a867af8539f
nisrinkh <204346333+nisrinkh@users.noreply.github.com>
2025-11-28 10:44:22 +0700

    tampilan model mobil admin (nambah upload gambar)

--- resources/js/pages/Admin/ModelMobil/Index.vue
+++ resources/js/pages/Admin/ModelMobil/Index.vue
@@ -14,6 +14,7 @@
       <thead class="bg-gray-200">
         <tr>
           <th class="p-2 text-left">#</th>
+          <th class="p-2 text-left">Gambar</th>
           <th class="p-2 text-left">Brand</th>
           <th class="p-2 text-left">Model</th>
           <th class="p-2 text-left">Jml. Sparepart</th>
@@ -23,6 +24,17 @@
       <tbody>
         <tr v-for="(item, i) in modelMobils" :key="item.id" class="border-b">
           <td class="p-2">{{ i + 1 }}</td>
+
+          <!-- Tampilkan gambar -->
+          <td class="p-2">
+            <img 
+              v-if="item.image"
+              :src="getImageUrl(item.image)"
+              class="w-16 h-12 object-contain rounded border bg-white"
+            />
+            <span v-else class="text-gray-400 text-xs italic">No Image</span>
+          </td>
+
           <td class="p-2">{{ item.brand }}</td>
           <td class="p-2">{{ item.model }}</td>
           <td class="p-2">{{ item.spareparts ? item.spareparts.length : 0 }}</td>
@@ -42,15 +54,17 @@
 import { ref, onMounted } from 'vue'
 import axios from 'axios'
 
-// Variabel diubah namanya menjadi modelMobils
 const modelMobils = ref([])
 
-// Fungsi formatCurrency dihapus karena tidak digunakan lagi
+const getImageUrl = (path) => {
+  return path ? `/storage/${path}` : ''
+}
 
 const loadData = async () => {
   try {
     const res = await axios.get('/api/admin/models')
-    modelMobils.value = res.data
+    const sortedData = res.data.sort((a, b) => b.id - a.id)
+    modelMobils.value = sortedData
   } catch (error) {
     console.error('Gagal memuat data model mobil:', error)
   }
