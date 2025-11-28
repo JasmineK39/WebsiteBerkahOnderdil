8430c51642e3f6c31138f176a17308f24331c94a
nisrinkh <204346333+nisrinkh@users.noreply.github.com>
2025-11-28 10:47:45 +0700

    seeder

--- database/seeders/MobilSeeder.php
+++ database/seeders/MobilSeeder.php
@@ -11,14 +11,6 @@ class MobilSeeder extends Seeder
     {
         $mobils = [
             ['brand' => 'Toyota', 'model' => 'Kijang Innova', 'image' => 'kijangInova.png'],
-            ['brand' => 'Daihatsu', 'model' => 'Avanza', 'image' => 'DaihatsuAvanza.png'],
-            ['brand' => 'Honda', 'model' => 'Jazz', 'image' => 'Honda-Jazz.png'],
-            ['brand' => 'Suzuki', 'model' => 'Swift', 'image' => 'Suzuki_Swift.png'],
-            ['brand' => 'Mitsubishi', 'model' => 'Xpander', 'image' => 'Mitsubishi_Xpander.png'],
-            ['brand' => 'Nissan', 'model' => 'Livina', 'image' => 'Nissan_Livina.png'],
-            ['brand' => 'Ford', 'model' => 'EcoSport', 'image' => 'Ford_EcoSport.png'],
-            ['brand' => 'Chevrolet', 'model' => 'Captiva', 'image' => 'Chevrolet-Captiva.png'],
-            ['brand' => 'BMW', 'model' => 'X1', 'image' => ''],
             
         ];
 
