5e4bbe567be6972db42cc7703deaff61d6748851
Jasmine <jasmine@example.com>
2025-12-04 09:10:35 +0700

    tambah data model mobil

--- database/seeders/SparepartSeeder.php
+++ database/seeders/SparepartSeeder.php
@@ -13,24 +13,7 @@ class SparepartSeeder extends Seeder
     {
         $spareparts = [
             ['name' => 'Kampas Rem Depan', 'brand' => 'Brembo', 'type' => 'Rem', 'grade' => 'A', 'price' => 450000, 'image' => ''],
-            ['name' => 'Kampas Rem Belakang', 'brand' => 'Brembo', 'type' => 'Rem', 'grade' => 'B', 'price' => 350000],
-            ['name' => 'Rotor Rem', 'brand' => 'Brembo', 'type' => 'Rem', 'grade' => 'A', 'price' => 750000],
-            ['name' => 'Minyak Rem DOT4', 'brand' => 'Prestone', 'type' => 'Rem', 'grade' => 'C', 'price' => 95000],
-            ['name' => 'Kaliper Rem', 'brand' => 'TRW', 'type' => 'Rem', 'grade' => 'A', 'price' => 600000],
-
-            ['name' => 'Filter Oli', 'brand' => 'Denso', 'type' => 'Mesin', 'grade' => 'A', 'price' => 75000],
-            ['name' => 'Busi Iridium', 'brand' => 'NGK', 'type' => 'Mesin', 'grade' => 'C', 'price' => 120000],
-            ['name' => 'Radiator', 'brand' => 'Denso', 'type' => 'Mesin', 'grade' => 'A', 'price' => 1200000],
-            ['name' => 'Thermostat', 'brand' => 'Aisin', 'type' => 'Mesin', 'grade' => 'B', 'price' => 250000],
-
-            ['name' => 'Kopling Set', 'brand' => 'Exedy', 'type' => 'Transmisi', 'grade' => 'A', 'price' => 950000],
-            ['name' => 'Oli Transmisi', 'brand' => 'Shell', 'type' => 'Transmisi', 'grade' => 'B', 'price' => 185000],
-
-            ['name' => 'Shockbreaker Depan', 'brand' => 'KYB', 'type' => 'Suspensi', 'grade' => 'C', 'price' => 850000],
-            ['name' => 'Bushing Arm', 'brand' => '555', 'type' => 'Suspensi', 'grade' => 'A', 'price' => 220000],
-
-            ['name' => 'Baterai Aki', 'brand' => 'GS Astra', 'type' => 'Kelistrikan', 'grade' => 'A', 'price' => 950000],
-            ['name' => 'Lampu Depan LED', 'brand' => 'Philips', 'type' => 'Kelistrikan', 'grade' => 'C', 'price' => 450000],
+            
         ];
 
         $mobils = ModelMobil::all();
